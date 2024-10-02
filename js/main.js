2fe4d27d18604ebc809d49d45a7e8317fce89cc2
phuocmanh8701 <chienthan11456@gmail.com>
2024-10-02 16:41:51 +0700

    update 2/10/2024

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 /**
- 
- * 
- 
+
+ *
+
 **/
 
 (function ($) {
     ("use strict");
 
-    
 
+    var videoWrap = function () {
+        if ($("div").hasClass("video-wrap")) {
+            $(".popup-youtube").magnificPopup({
+                type: "iframe",
+            });
+        }
+    };
     // Dom Ready
     $(function () {
-       
+        videoWrap();
     });
 })(jQuery);
