92763a26beefa50231c63f28deb3253e1b718272
phuocmanh8701 <chienthan11456@gmail.com>
2024-10-03 17:01:02 +0700

    3/10/2024

--- js/main.js
+++ js/main.js
@@ -15,8 +15,44 @@
             });
         }
     };
+
+    var openNavMobile = () => {
+        if ($('.header').hasClass('header')) {
+
+            $('.mobile-button').on("click", () => {
+                $('.mobile-nav-wrap').toggleClass("active")
+                $('body').toggleClass('no-scroll');
+            })
+
+            $('.overlay-mobile-nav').on("click", () => {
+                $('.mobile-nav-wrap').toggleClass("active")
+                $('body').toggleClass('no-scroll');
+
+            })
+            $(".mobile-nav-close").on('click', () => {
+                $('.mobile-nav-wrap').toggleClass("active")
+                $('body').toggleClass('no-scroll');
+            })
+
+            $(document).on("click", ".menu-item-has-children-mobile", function () {
+                var args = { duration: 200 };
+                if ($(this).hasClass("active")) {
+                    $(this).children(".sub-menu-mobile").slideUp(args);
+                    $(this).removeClass("active");
+                } else {
+                    $(".sub-menu-mobile").slideUp(args);
+                    $(this).children(".sub-menu-mobile").slideDown(args);
+                    $(".menu-item-has-children-mobile").removeClass("active");
+                    $(this).addClass("active");
+                }
+            });
+
+
+        }
+    }
     // Dom Ready
     $(function () {
         videoWrap();
+        openNavMobile();
     });
 })(jQuery);
