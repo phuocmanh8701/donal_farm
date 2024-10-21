0c2cd2f5e0aaf8ba8749f712f31db4786f39b26b
phuocmanh8701 <phuocmanh8701@gmail.com>
2024-10-21 17:03:07 +0700

    21/10/20247

--- js/swiper.js
+++ js/swiper.js
@@ -10,10 +10,10 @@ if ($(".slider-home-1").length > 0) {
     var swiper = new Swiper(".slider-home-1", {
         slidesPerView: 1,
         loop: true,
-        // autoplay: {
-        //     delay: 4000,  // Thời gian giữa mỗi lần chuyển slide
-        //     disableOnInteraction: false,  // Để autoplay không bị dừng khi tương tác với Swiper
-        // },
+        autoplay: {
+            delay: 4000, // Thời gian giữa mỗi lần chuyển slide
+            disableOnInteraction: false, // Để autoplay không bị dừng khi tương tác với Swiper
+        },
         navigation: {
             nextEl: ".btn-slide-home-1.btn-next", // Chọn phần tử chứa nút next
             prevEl: ".btn-slide-home-1.btn-prev", // Chọn phần tử chứa nút prev
@@ -28,10 +28,10 @@ if (".slider-home-2".length > 0) {
             nextEl: ".btn-slide-home-2.btn-next", // Chọn phần tử chứa nút next
             prevEl: ".btn-slide-home-2.btn-prev", // Chọn phần tử chứa nút prev
         },
-        autoplay: {
-            delay: 4000, // Thời gian giữa mỗi lần chuyển slide
-            disableOnInteraction: false, // Để autoplay không bị dừng khi tương tác với Swiper
-        },
+        // autoplay: {
+        //     delay: 4000, // Thời gian giữa mỗi lần chuyển slide
+        //     disableOnInteraction: false, // Để autoplay không bị dừng khi tương tác với Swiper
+        // },
         on: {
             slideChange: function () {
                 $(".swiper-slide-active .tlt").each(function () {
@@ -62,10 +62,10 @@ if (".slider-home-3".length > 0) {
     var swiper = new Swiper(".slider-home-3", {
         slidesPerView: 1,
         loop: true,
-        autoplay: {
-            delay: 4000, // Thời gian giữa mỗi lần chuyển slide
-            disableOnInteraction: false, // Để autoplay không bị dừng khi tương tác với Swiper
-        },
+        // autoplay: {
+        //     delay: 4000, // Thời gian giữa mỗi lần chuyển slide
+        //     disableOnInteraction: false, // Để autoplay không bị dừng khi tương tác với Swiper
+        // },
         navigation: {
             nextEl: ".btn-slide-home-3.btn-next", // Chọn phần tử chứa nút next
             prevEl: ".btn-slide-home-3.btn-prev", // Chọn phần tử chứa nút prev
